bca1ee57aeed98975d7bf7be27316b4087272787
Rafael Vera <rafaeldejesusverareyes@gmail.com>
2022-06-08 22:13:48 -0500

    Create index.php
    Se agrega la vista de los proyectos

---
+++ home/proyecto/index.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['correo_usr'])) {
+    header("Location: ../../");
+}
+
+if(!isset($_GET['id'])) {
+    header("Location: ../");
+}
+
+require_once '../../database/connection.php';
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Task Assigner</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
+        <?php
+        require_once '../../includes/navbar.php';
+        
+        $puesto = get_puesto(
+            $_SESSION['correo_usr'],
+            $_GET['id']
+        );
+
+        if($puesto == null) {
+            header("Location: ../");
+        }
+
+        switch ($puesto) {
+            case "Administrador":
+                // Vista administrador
+                require_once 'administrador.php';
+                break;
+            case "Sub Administrador":
+                // Vista sub-administrador
+                require_once 'sub-administrador.php';
+                break;
+            case "Empleado":
+                // Vista empleado
+                require_once 'empleado.php';
+                break;
+        }
+        ?>
+    </body>
+    <?php
+    require_once '../../includes/footer.php';
+    ?>
+</html>
