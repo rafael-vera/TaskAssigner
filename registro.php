0e23775e2bb04695bb0c64704a0bf8ae9be5eace
Rafael Vera <rafaeldejesusverareyes@gmail.com>
2022-06-08 20:16:57 -0500

    Update registro.php
    Actualizado el diseño

--- registro.php
+++ registro.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 
-require_once 'database/connection.php';
-
 if (isset($_SESSION['correo_usr'])) {
     header("Location: home/");
 }
+
+require_once 'database/connection.php';
 
 if (isset($_POST['submit'])) {
     if($_POST['contrasena_usr'] != $_POST['confirm_contrasena_usr']) {
@@ -36,64 +36,60 @@ if (isset($_POST['submit'])) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </head>
     <body>
-        <nav class="navbar navbar-light bg-primary fixed-top">
-            <div class="container-fluid">
-                <a href="/taskassigner/" class="navbar-brand text-white">Task Assigner</a>
-            </div>
-        </nav>
+        <?php
+        require_once 'includes/navbar.php';
+        ?>
         
-        <section class="vh-100">
-            <div class="container py-5 h-100">
-                <div class="row d-flex justify-content-center align-items-center h-100">
-                    <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                        <div class="card shadow-lg bg-light">
-                            <form action="" method="POST" class="card-body p-5 text-center">
-                                <h3 class="mb-5">Registro</h3>
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card shadow-lg bg-light">
+                        <form action="" method="POST" class="card-body p-5 text-center">
+                            <h3 class="mb-5">Registro</h3>
 
-                                <div class="row">
-                                    <div class="col-md-6 mb-3">
-                                        <div class="form-floating">
-                                            <input type="text" class="form-control" id="floating-name" name="nombre_usr" required>
-                                            <label for="floating-name">Nombre</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6 mb-3">
-                                        <div class="form-floating">
-                                            <input type="text" class="form-control" id="floating-lastname" name="apellidos_usr" required>
-                                            <label for="floating-lastname">Apellidos</label>
-                                        </div>
+                            <div class="row">
+                                <div class="col-md-6 mb-3">
+                                    <div class="form-floating">
+                                        <input type="text" class="form-control" id="floating-name" name="nombre_usr" required>
+                                        <label for="floating-name">Nombre</label>
                                     </div>
                                 </div>
-
-                                <div class="form-floating mb-3">
-                                    <input type="email" class="form-control" id="floating-email" name="correo_usr" required>
-                                    <label for="floating-email">Correo</label>
+                                <div class="col-md-6 mb-3">
+                                    <div class="form-floating">
+                                        <input type="text" class="form-control" id="floating-lastname" name="apellidos_usr" required>
+                                        <label for="floating-lastname">Apellidos</label>
+                                    </div>
                                 </div>
+                            </div>
 
-                                <div class="form-floating mb-3">
-                                    <input type="password" class="form-control" id="floating-password" name="contrasena_usr" required>
-                                    <label for="floating-password">Contraseña</label>
-                                </div>
+                            <div class="form-floating mb-3">
+                                <input type="email" class="form-control" id="floating-email" name="correo_usr" required>
+                                <label for="floating-email">Correo</label>
+                            </div>
 
-                                <div class="form-floating mb-3">
-                                    <input type="password" class="form-control" id="floating-password" name="confirm_contrasena_usr" required>
-                                    <label for="floating-password">Confirma contraseña</label>
-                                </div>
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control" id="floating-password" name="contrasena_usr" required>
+                                <label for="floating-password">Contraseña</label>
+                            </div>
 
-                                <button class="btn btn-primary btn-lg btn-block" name="submit" type="submit">Registrarse</button>
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control" id="floating-password" name="confirm_contrasena_usr" required>
+                                <label for="floating-password">Confirma contraseña</label>
+                            </div>
 
-                                <hr class="my-4">
-                                <div>
-                                    <p class="mb-0">¿Ya tienes una cuenta? <a href="index.php">Inicia sesión</a></p>
-                                </div>
-                            </form>
-                        </div>
+                            <button class="btn btn-primary btn-lg btn-block" name="submit" type="submit">Registrarse</button>
+
+                            <hr class="my-4">
+                            <div>
+                                <p class="mb-0">¿Ya tienes una cuenta? <a href="index.php">Inicia sesión</a></p>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
     </body>
     <?php
-    require_once 'includes/footer.html';
+    require_once 'includes/footer.php';
     ?>
 </html>
