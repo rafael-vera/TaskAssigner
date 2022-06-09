cdcc435c50a2b0da5e62da15e566281b17ca8ee3
Rafael Vera <rafaeldejesusverareyes@gmail.com>
2022-06-08 20:16:31 -0500

    Create no-projects.php
    Vista en caso de que no haya proyectos

---
+++ home/no-projects.php
@@ -0,0 +1,6 @@
+<main class="container p-5">
+    <h1 class="text-center">No hay proyectos</h1>
+    <div class="d-flex justify-content-center m-5">
+        <a href="new-project.php" class="btn btn-success p-4 fs-5">Crear nuevo proyecto</a>
+    </div>
+</main>
