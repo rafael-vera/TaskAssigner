a9fe32fc693b8949672a1d4353d20a05fd410417
Rafael Vera <rafaeldejesusverareyes@gmail.com>
2022-06-09 23:35:08 -0500

    Update grid-cards.php
    Se cambia la vista de los proyectos

--- home/grid-cards.php
+++ home/grid-cards.php
@@ -13,13 +13,13 @@
         $cont = 3;
         while($cont > 0 && $num_projects > 0) {
             echo '
-            <div class="col d-flex justify-content-evenly">
-                <div class="card w-50 shadow">
+            <div class="col mx-5 d-flex justify-content-center">
+                <div class="card w-100 shadow">
                     <div class="card-body">
                         <h5 class="card-title">'.$projects['nombre'][$i].'</h5>
                         <p class="card-text">'.$projects['descripcion'][$i].'</p>
                         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <a href="proyecto/?id='.$projects['id'][$i].'" class="btn btn-info">Ir al proyecto</a>
+                            <a href="project/?id='.$projects['id'][$i].'" class="btn btn-info">Ir al proyecto</a>
                         </div>
                     </div>
                 </div>
