d7aa1c02a0fd2f5c71819fca5b281865ceabf4d2
Rafael Vera <rafaeldejesusverareyes@gmail.com>
2022-06-12 15:56:59 -0500

    Update formularios
    Se actualiza el diseño

--- home/project/members/add.php
+++ home/project/members/add.php
@@ -14,33 +14,33 @@ if(!isset($_GET['id'])) {
 
 require_once '../../../database/connection.php';
 
-    $puesto = get_puesto(
-        $_SESSION['correo_usr'],
-        $_GET['id']
-    );
-    // Si el usuario no pertenece al proyecto lo regresa a home
-    if($puesto == null) {
-        header("Location: ../../");
-    }
-    // Si el usuario no es administrador o sub administrador del proyecto lo regresa a project
-    if(strcmp($puesto, "Empleado") == 0) {
-        header("Location: ../?id=".$_GET['id']);
-    }
+$puesto = get_puesto(
+    $_SESSION['correo_usr'],
+    $_GET['id']
+);
+// Si el usuario no pertenece al proyecto lo regresa a home
+if($puesto == null) {
+    header("Location: ../../");
+}
+// Si el usuario no es administrador o sub administrador del proyecto lo regresa a project
+if(strcmp($puesto, "Empleado") == 0) {
+    header("Location: ../?id=".$_GET['id']);
+}
 
-    if (isset($_POST['submit'])) {
-        if($_POST['puesto']!=1){
-            $val = add_integrante(
-                $_POST['correo_usr'], 
-                $_POST['puesto'], 
-                $_GET['id']);
-            if($val==true){
-                header("Location: index.php?id=".$_GET['id']);
-            } else{
-                echo "<script>alert('Error al realizar el registro')</script>";
-            }
+if (isset($_POST['submit'])) {
+    if($_POST['puesto']!=1){
+        $val = add_integrante(
+            $_POST['correo_usr'],
+            $_POST['puesto'],
+            $_GET['id']
+        );
+        if($val){
+            header("Location: index.php?id=".$_GET['id']);
+        } else{
+            echo "<script>alert('Error al realizar el registro')</script>";
         }
     }
-
+}
 ?>
 
 <!DOCTYPE html>
@@ -55,42 +55,44 @@ require_once '../../../database/connection.php';
         require_once '../../../includes/navbar.php';
         ?>
         
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                    <div class="card shadow-lg bg-light">
-                        <form action="" method="POST" class="card-body p-5 text-center">
-                            <h3 class="mb-5">Nuevo usuario del proyecto</h3>
+        <section class="vh-75">
+            <div class="container py-5">
+                <div class="row d-flex justify-content-center align-items-center">
+                    <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                        <div class="card shadow-lg bg-light">
+                            <form action="" method="POST" class="card-body p-5 text-center">
+                                <h3 class="mb-5">Nuevo usuario del proyecto</h3>
 
-                            <div class="form-floating mb-3">
-                                <input type="email" class="form-control" id="floating-email" name="correo_usr" required>
-                                <label for="floating-email">Correo</label>
-                            </div>
+                                <div class="form-floating mb-3">
+                                    <input type="email" class="form-control" id="floating-email" name="correo_usr" required>
+                                    <label for="floating-email">Correo</label>
+                                </div>
 
-                            <div class="row">
-                            <div class="col-md-2 mb-3 my-auto">
-                            <label>
-                                Puesto
-                            </label>
-                            </div>
-                            <div class="col-md-10 mb-3">
-                            <select class="form-select my-auto" aria-label="Default select example" name="puesto">
-                                <option value="2">Sub Administrador</option>
-                                <option value="3">Empleado</option>
-                            </select>
-                            </div>
-                            </div>
+                                <div class="row">
+                                <div class="col-md-2 mb-3 my-auto">
+                                <label>
+                                    Puesto
+                                </label>
+                                </div>
+                                <div class="col-md-10 mb-3">
+                                <select class="form-select my-auto" aria-label="Default select example" name="puesto">
+                                    <option value="2">Sub Administrador</option>
+                                    <option value="3">Empleado</option>
+                                </select>
+                                </div>
+                                </div>
 
-                            <div class="d-flex justify-content-evenly">
-                                    <?php echo '<a href="index.php?id='.$_GET['id'].'" class="btn btn-danger btn-lg btn-block">Cancelar</a>';?>
-                                    <button class="btn btn-primary btn-lg btn-block" name="submit" type="submit">Registrar</button>
-                            </div>
-                            
-                        </form>
+                                <div class="d-flex justify-content-evenly">
+                                        <?php echo '<a href="index.php?id='.$_GET['id'].'" class="btn btn-danger btn-lg btn-block">Cancelar</a>';?>
+                                        <button class="btn btn-primary btn-lg btn-block" name="submit" type="submit">Registrar</button>
+                                </div>
+                                
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     </body>
     
     <?php
