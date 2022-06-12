f08f95bd0af42b9eeea27bbf9dbde13af375143d
Rafael Vera <rafaeldejesusverareyes@gmail.com>
2022-06-12 03:37:35 -0500

    Update index.php
    Se agregó para poder poner iconos

--- home/project/index.php
+++ home/project/index.php
@@ -18,6 +18,7 @@ require_once '../../database/connection.php';
         <meta charset="UTF-8">
         <title>Task Assigner</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
         <?php
