a285498a2a529d51ac88219d5f56143fb8e55b29
Rafael Vera <rafaeldejesusverareyes@gmail.com>
2022-06-12 03:38:34 -0500

    Update delete.php
    Se agregó la funcionalidad para eliminar integrantes

--- home/project/members/delete.php
+++ home/project/members/delete.php
@@ -1,1 +1,42 @@
 <!-- Script que elimina un integrante de un proyecto -->
+<?php
+session_start();
+
+if (!isset($_SESSION['correo_usr'])) {
+    header("Location: ../../../");
+}
+
+if(!isset($_GET['id'])) {
+    header("Location: ../../");
+}
+
+if(!isset($_GET['usr'])) {
+    header("Location: index.php?id=".$_GET['id']);
+}
+
+require_once '../../../database/connection.php';
+
+$puesto = get_puesto(
+    $_SESSION['correo_usr'],
+    $_GET['id']
+);
+// Si el usuario no pertenece al proyecto lo regresa a home
+if($puesto == null) {
+    header("Location: ../../");
+}
+// Si el usuario no es administrador o sub administrador del proyecto lo regresa a project
+if(strcmp($puesto, "Empleado") == 0) {
+    header("Location: ../?id=".$_GET['id']);
+}
+
+// Si todo es correcto, elimina al usuario
+$eliminar = delete_integrante(
+    $_GET['usr'],
+    $_GET['id']
+);
+
+if($eliminar) {
+    echo "<script>alert('Error al eliminar al integrante')</script>";
+}
+header("Location: index.php?id=".$_GET['id']);
+?>
