04849df7891b5a00771cd54b26a3dc82b51317e6
Rafael Vera <rafaeldejesusverareyes@gmail.com>
2022-06-13 15:26:19 -0500

    Update no-projects.php
    Icono agregado

--- home/no-projects.php
+++ home/no-projects.php
@@ -1,6 +1,8 @@
 <main class="container p-5">
     <h1 class="text-center">No hay proyectos</h1>
     <div class="d-flex justify-content-center m-5">
-        <a href="new-project.php" class="btn btn-success p-4 fs-5">Crear nuevo proyecto</a>
+        <a href="new-project.php" class="btn btn-success p-4 fs-5">
+            <i class="fa fa-plus-circle" aria-hidden="true"> Crear nuevo proyecto</i>
+        </a>
     </div>
 </main>
