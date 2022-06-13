746ab73eb4906a41b11be81fd8f77ce279bfa378
Rafael Vera <rafaeldejesusverareyes@gmail.com>
2022-06-13 03:32:10 -0500

    Update member.php
    Vista para ver la información y tareas de un integrante

--- home/project/members/member.php
+++ home/project/members/member.php
@@ -20,6 +20,19 @@ if(!isset($_GET['usr'])) {
 }
 
 require_once '../../../database/connection.php';
+
+if (isset($_POST['submit'])) {
+    $val = actualizar_puesto(
+        $_GET['usr'],
+        $_GET['id'],
+        $_POST['puesto']
+    );
+    if($val){
+        header("Location: index.php?id=".$_GET['id']);
+    } else{
+        echo "<script>alert('Error al modificar el puesto')</script>";
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -38,12 +51,239 @@ require_once '../../../database/connection.php';
             $_SESSION['correo_usr'],
             $_GET['id']
         );
-
+        
         if($puesto == null) {
-            header("Location: ../");
+            header("Location: ../../");
         }
+
         if(strcmp($puesto, "Empleado") == 0) {
             header("Location: ../");
+        }
+
+        $puestoEmpleado = get_puesto(
+            $_GET['usr'],
+            $_GET['id']
+        );
+
+        $usuario = get_usuario($_GET['usr']);
+        ?>
+        
+        <section class="vh-75">
+            <div class="container py-5">
+                <div class="row d-flex justify-content-center align-items-center">
+                    <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                        <div class="card shadow-lg bg-light">
+                            <?php
+                            if(strcmp($puestoEmpleado, "Administrador") == 0) {
+                                echo '<div class="card-body p-5 text-center">';
+                            } else {
+                                echo '<form action="" method="POST" class="card-body p-5 text-center">';
+                            }
+                            ?>
+                                <h3 class="mb-5">Datos del integrante</h3>
+
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="floating-nombre" name="nombre_usr" disabled required value="<?php echo $usuario["nombre_usr"]." ".$usuario["apellidos_usr"];?>">
+                                    <label for="floating-nombre">Nombre</label>
+                                </div>
+
+                                <div class="form-floating mb-3">
+                                    <input type="email" class="form-control" id="floating-email" name="correo" disabled required value="<?php echo $usuario["correo_usr"];?>">
+                                    <label for="floating-email">Correo</label>
+                                </div>
+
+                            <?php
+                            if(strcmp($puestoEmpleado, "Administrador") == 0) {
+                                echo '
+                                    <div class="form-floating mb-3">
+                                        <input type="text" class="form-control" id="floating-puesto" name="puesto" disabled required value="'.$puestoEmpleado.'">
+                                        <label for="floating-puesto">Puesto</label>
+                                    </div>
+                                    
+                                    <a href="index.php?id='.$_GET['id'].'" class="btn btn-secondary btn-lg btn-block">Volver</a>
+                                </div>
+                                ';
+                            } else {
+                                echo '
+                                    <div class="d-flex justify-content-evenly my-3">
+                                        <div class="my-auto px-3">
+                                            <label justify-content-center>
+                                                Puesto:
+                                            </label>
+                                        </div>
+                                        <select class="form-select" aria-label="Default select example" name="puesto">
+                                            <option value="2" ';
+                                if(strcmp($puestoEmpleado, "Sub Administrador") == 0) {
+                                    echo 'selected';
+                                }
+                                echo '          >Sub Administrador</option>
+                                            <option value="3" ';
+                                if(strcmp($puestoEmpleado, "Empleado") == 0) {
+                                    echo 'selected';
+                                }
+                                echo '      >Empleado</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="d-flex justify-content-evenly">
+                                        <a href="index.php?id='.$_GET['id'].'" class="btn btn-secondary btn-lg btn-block">Volver</a>
+                                        <button class="btn btn-primary btn-lg btn-block" name="submit" type="submit">Actualizar</button>
+                                    </div>
+                                </form>
+                                ';
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <?php
+        if(strcmp($puestoEmpleado, "Administrador") != 0) {
+            // Obtener tareas para este usuario
+            $tareas = get_tareas(
+                $_GET['usr'],
+                $_GET['id']
+            );
+
+            $pendientes = array(
+                'nombre'=>array(),
+                'descripcion'=>array(),
+                'fecha'=>array(),
+                'terminado'=>array()
+            );
+            $completadas = array(
+                'nombre'=>array(),
+                'descripcion'=>array(),
+                'fecha'=>array(),
+                'terminado'=>array()
+            );
+            $atrasadas = array(
+                'nombre'=>array(),
+                'descripcion'=>array(),
+                'fecha'=>array(),
+                'terminado'=>array()
+            );
+
+            $num_total = count($tareas['nombre']);
+            if($num_total > 0) {
+                $i = 0;
+                $fecha_actual = new DateTime();
+                $fecha_actual = $fecha_actual->format('Y-m-d');
+                while($i < $num_total) {
+                    $fecha_tarea = new DateTime($tareas['fecha'][$i]);
+                    $fecha_tarea = $fecha_tarea->format('Y-m-d');
+                    if($tareas['terminado'][$i] != 0) {
+                        array_push($completadas['nombre'], $tareas['nombre'][$i]);
+                        array_push($completadas['descripcion'], $tareas['descripcion'][$i]);
+                        array_push($completadas['fecha'], $tareas['fecha'][$i]);
+                        array_push($completadas['terminado'], $tareas['terminado'][$i]);
+                    } else if($fecha_actual > $fecha_tarea) {
+                        array_push($atrasadas['nombre'], $tareas['nombre'][$i]);
+                        array_push($atrasadas['descripcion'], $tareas['descripcion'][$i]);
+                        array_push($atrasadas['fecha'], $tareas['fecha'][$i]);
+                        array_push($atrasadas['terminado'], $tareas['terminado'][$i]);
+                    } else {
+                        array_push($pendientes['nombre'], $tareas['nombre'][$i]);
+                        array_push($pendientes['descripcion'], $tareas['descripcion'][$i]);
+                        array_push($pendientes['fecha'], $tareas['fecha'][$i]);
+                        array_push($pendientes['terminado'], $tareas['terminado'][$i]);
+                    }
+                    $i++;
+                }
+            }
+
+            echo '
+            <main class="container p-5 mb-5" id="tareas">
+                <div class="d-flex justify-content-between">
+                    <h2>Tareas</h2>
+                    <div class="my-auto">
+                        <a href="add-task.php?id='.$_GET['id'].'&usr='.$_GET['usr'].'" class="btn btn-success">Agregar tarea</a>
+                    </div>
+                </div>
+                <hr>
+                <div class="row mx-2 d-flex justify-content-evenly">
+                    <div class="card shadow col mx-3" style="background-color: #fff054;">
+                        <h4 class="mt-3 text-center">
+                            Pendientes
+                            <hr>
+                        </h4>
+                        <div style="overflow-x: hidden; overflow-y: auto; height: 400px;">';
+            $i = 0;
+            $total = count($pendientes['nombre']);
+            while($i < $total) {
+                echo '
+                            <div class="card bg-light m-1">
+                                <h6 class="card-header">'.$pendientes['nombre'][$i].'</h6>
+                                <div class="card-body">
+                                    <p>'.$pendientes['descripcion'][$i].'</p>
+                                    <div class="d-flex justify-content-between">
+                                        <p class="my-auto">'.$pendientes['fecha'][$i].'</p>
+                                        <a href="task-done.php?id='.$_GET['id'].'&usr='.$_GET['usr'].'&task='.$pendientes['nombre'][$i].'" class="btn btn-success"><i class="fa fa-check text-white" aria-hidden="true"> Realizado</i></a>
+                                    </div>
+                                </div>
+                            </div>
+                ';
+                $i++;
+            }
+            echo '
+                        </div>
+                    </div>
+                    <div class="card shadow col mx-3" style="background-color: #44a97a;">
+                        <h4 class="mt-3 text-center">
+                            Completadas
+                            <hr>
+                        </h4>
+                        <div style="overflow-x: hidden; overflow-y: auto; height: 400px;">';
+            $i = 0;
+            $total = count($completadas['nombre']);
+            while($i < $total) {
+                echo '
+                            <div class="card bg-light m-1">
+                                <h6 class="card-header">'.$completadas['nombre'][$i].'</h6>
+                                <div class="card-body">
+                                    <p>'.$completadas['descripcion'][$i].'</p>
+                                    <div class="d-flex justify-content-between">
+                                        <p class="my-auto">'.$completadas['fecha'][$i].'</p>
+                                        <a href="task-undone.php?id='.$_GET['id'].'&usr='.$_GET['usr'].'&task='.$completadas['nombre'][$i].'" class="btn btn-danger"><i class="fa fa-times" aria-hidden="true"> No realizado</i></a>
+                                    </div>
+                                </div>
+                            </div>
+                ';
+                $i++;
+            }
+            echo '
+                        </div>
+                    </div>
+                    <div class="card shadow col mx-3" style="background-color: #f65564;">
+                        <h4 class="mt-3 text-center">
+                            Atrasadas
+                            <hr>
+                        </h4>
+                        <div style="overflow-x: hidden; overflow-y: auto; height: 400px;">';
+            $i = 0;
+            $total = count($atrasadas['nombre']);
+            while($i < $total) {
+                echo '
+                            <div class="card bg-light m-1">
+                                <h6 class="card-header">'.$atrasadas['nombre'][$i].'</h6>
+                                <div class="card-body">
+                                    <p>'.$atrasadas['descripcion'][$i].'</p>
+                                    <div class="d-flex justify-content-between">
+                                        <p class="my-auto">'.$atrasadas['fecha'][$i].'</p>
+                                        <a href="task-done.php?id='.$_GET['id'].'&usr='.$_GET['usr'].'&task='.$atrasadas['nombre'][$i].'" class="btn btn-success"><i class="fa fa-check text-white" aria-hidden="true"> Realizado</i></a>
+                                    </div>
+                                </div>
+                            </div>
+                ';
+                $i++;
+            }
+            echo '
+                        </div>
+                    </div>
+                </div>
+            </main>
+            ';
         }
         ?>
     </body>
