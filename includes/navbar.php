625d304b827355dfc6d9e4cfa411bd71728046a2
Rafael Vera <rafaeldejesusverareyes@gmail.com>
2022-06-15 03:53:14 -0500

    Update navbar.php
    Se agrega la opción de ver el perfil del usuario

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,13 @@
 <nav class="navbar navbar-light bg-primary sticky-top">
     <div class="container-fluid">
-        <a href="/taskassigner/" class="navbar-brand text-white">Task Assigner</a>
+        <a href="/taskassigner" class="navbar-brand text-white">Task Assigner</a>
         <?php
         if (isset($_SESSION['correo_usr'])) {
             echo '
-            <a href="/taskassigner/logout.php" class="navbar-text navbar-brand text-white">Cerrar sesión</a>
+            <div>
+                <a href="/taskassigner/home/user/" class="navbar-text navbar-brand text-white mx-4 px-3 border border-white border-2 rounded-pill">Ver perfil</a>
+                <a href="/taskassigner/logout.php" class="navbar-text navbar-brand text-white px-3 border border-white border-2 rounded-pill">Cerrar sesión</a>
+            </div>
             ';
         }
         ?>
