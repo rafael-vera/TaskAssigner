c4229908757d11123b726b38ad3b87c21297eb47
Rafael Vera <rafaeldejesusverareyes@gmail.com>
2022-06-15 03:51:35 -0500

    Update new-project.php
    Se agregan validaciones al formulario

--- home/new-project.php
+++ home/new-project.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION['correo_usr'])) {
-    header("Location: ../home/index.php");
+    header("Location: ../");
 }
 
 require_once '../database/connection.php';
@@ -44,7 +44,7 @@ if (isset($_POST['submit'])) {
                                 <div class="row">
                                     <div class="col-md-12 mb-3">
                                         <div class="form-floating">
-                                            <input type="text" class="form-control" id="floating-name" name="nom_proyecto" required>
+                                            <input type="text" class="form-control" id="floating-name" name="nom_proyecto" maxlength="50" required>
                                             <label for="floating-name">Nombre del Proyecto</label>
                                         </div>
                                     </div>
@@ -53,13 +53,13 @@ if (isset($_POST['submit'])) {
                                 <div class="row">
                                     <div class="col-md-12 mb-3">
                                         <div class="form-floating">
-                                            <textarea type="text" class="form-control " id="floating-desc" name="desc_proyecto" required rows="20"></textarea>
+                                            <textarea type="text" class="form-control " id="floating-desc" name="desc_proyecto" maxlength="255" required rows="30" style="height: 160px"></textarea>
                                             <label for="floating-desc">Descripción del Proyecto</label>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="d-flex justify-content-evenly">
-                                    <a href="../" class="btn btn-danger btn-lg btn-block">Cancelar</a>
+                                    <a href="index.php" class="btn btn-danger btn-lg btn-block">Cancelar</a>
                                     <button class="btn btn-primary btn-lg btn-block" name="submit" type="submit">Crear Proyecto</button>
                                 </div>
                             </form>
